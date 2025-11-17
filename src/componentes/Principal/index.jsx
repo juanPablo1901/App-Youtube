55dc29a271773290cbd524fc235d82ba7c77208d
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2025-11-17 21:10:17 +0000

    actualizacion 1

--- src/componentes/Principal/index.jsx
+++ src/componentes/Principal/index.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { useEffect, useState } from 'react';
+import { FaRegUserCircle, FaUserCircle, FaYoutube, FaChromecast } from "react-icons/fa";
+
+import './style.css'
 
 
 function Principal (){
    return(
+
     <div className='Videos'>
 
-      <ReactPlayer 
+      <div>
+        <ReactPlayer 
       url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
       controls
       width='200px'
@@ -15,8 +20,26 @@ function Principal (){
       playing
       volume={0.5}
       onEnded={() => alert('Termino Video')}
+        />
+        <div className='Video-info'>
+          <div className='User-icon'>
+          <FaUserCircle size={25} />
+          </div>
+          <div className='Video-Nombre'>
+            video #1
+
+          </div>
+
+        
+        <diV className='Opciones'>
+
+        </diV>
+
+        </div>
+       
+      </div>
+
       
-      />
 
     </div>
 
